13626dfe92399cad89c9aa31a3b30f3b23464f69
Kevin Musiorski <rudyonrails@gmail.com>
2013-07-01 12:46:29 -0500

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flopbook::Application.config.secret_token = '02149d2274532d17c6f4aa194844f1db7963f96c60c316de13dd3ffbcbb80d1d70a6d556632a5d31d4048906c07ccb4e72d2c8adc23eccb53c56b76ded18dd20'
