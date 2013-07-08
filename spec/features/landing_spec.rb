21e644051c92c2dd7e361ffc3ef597017d95d357
Kevin Musiorski <rudyonrails@gmail.com>
2013-07-08 09:28:18 -0500

    adds spec for sign_up_link

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'landing page' do
   it "has 'sign up' link" do
     visit(root_path)
-    current_path.should == "/"
+    page.should have_selector("sign_up_link")
   end
 end
