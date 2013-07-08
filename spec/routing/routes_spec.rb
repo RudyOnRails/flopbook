914893312c216b891cd9a79195dffe6744540f65
Kevin Musiorski <rudyonrails@gmail.com>
2013-07-08 15:31:20 -0500

    converts application layout to haml... uncommented a routing spec... going to skip testing for now. didn't start soon enough on this, and time is running out...

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Flopbook" do
-  # it "routes to /" do
-  #   expect(:get => "/").to be_routable
-  # end
+  it "routes to /" do
+    expect(:get => "/").to be_routable
+  end
 end
