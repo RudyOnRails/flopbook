8d84b59376bc4356a5766f066fd5633921d6a449
Kevin Musiorski <rudyonrails@gmail.com>
2013-07-09 09:08:05 -0500

    makes sure proper attr's are accessible for mass assignment

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -6,7 +6,7 @@ class Owner < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :age, :location
   # attr_accessible :title, :body
 
   has_many :dogs
